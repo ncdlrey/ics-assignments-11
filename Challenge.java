8838a5e60aa726c26d166abf58423b762a22179f
Nicole <nicole.delreyes@gmail.com>
2022-04-09 20:40:15 -0400

    added Challenge.java

---
+++ Challenge.java
@@ -0,0 +1,31 @@
+
+public class Challenge {
+	public static void main(String[] args) {
+	    for (int i = 0; i < 10; ++i) {
+	         for (int j = 0; j < 10; ++j) 
+	 	        System.out.println(i + "," + j + " ");
+	 	     System.out.println();
+  	
+	 	        		
+	 	        	
+	     }
+	        
+	}
+			   	    
+} 
+
+
+			
+
+/*
+ *     for (int i = 0; i < 10; ++i) {
+      System.out.println(i + "," + 0 + " ");
+      
+       for (int j = 0; j < 10; ++j) {
+         System.out.print(i + "," + j + " ");
+       }
+      
+    }
+ */
+
+
