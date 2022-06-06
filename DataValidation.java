ff339147e2cfcc97875f247dc9ea2d829247b0d0
Nicole <nicole.delreyes@gmail.com>
2022-06-05 22:05:13 -0400

    coded all methods

--- DataValidation.java
+++ DataValidation.java
@@ -7,6 +7,8 @@ public class DataValidation {
 	static String stage2Input;
 	static String stage3Input;
 	static String stage4Input;
+	static String stage5Input;
+	static String stage6Input;
 	
 	public static void menu() {
 		
@@ -14,14 +16,7 @@ public class DataValidation {
 		System.out.println("                 DATA VALIDATION");
 		System.out.println(" Fill out and complete the following prompts!");
 		System.out.println("=============================================");
-		/*
-		System.out.println("Stage 1: The user must enter a string that is greater than 6 characters long.");
-		System.out.println("Stage 2: The user must enter a string that contains at least one lowercase “a”.");
-		System.out.println("Stage 3: The user must enter a string that is between 5 and 15 characters long, and does not contain the letter “z”.");
-		System.out.println("Stage 4: The user must enter an integer between 5 and 500.");
-		System.out.println("Stage 5: The user must enter a negative integer.");
-		System.out.println("Stage 6: The user must enter a positive, odd integer.");
-		*/
+		
 	}
 	
 	public static String realInt() {
@@ -46,12 +41,7 @@ public class DataValidation {
 	}
 	
 	
-	public static void stage1() {
-		System.out.println("\n                  STAGE 1");
-		System.out.println("                 ---------");
-		System.out.print("Enter a STRING that has more than 6 letters: ");
-		stage1Input = keyedInput.nextLine();
-		
+	public static void stage1(String stage1Input) {
 		while (stage1Input.length() <= 6) {
 			System.out.println("'" + stage1Input + "'" + " contains LESS than 6 characters.");
 			System.out.print("\nPlease re-enter: ");
@@ -60,13 +50,8 @@ public class DataValidation {
 			
 	}
 	
-	public static void stage2() {
-		System.out.println("\n                  STAGE 2");
-		System.out.println("                 ---------");
-		System.out.println("Enter a STRING that contains at least one lowercase “a”. ");
-		stage2Input = keyedInput.nextLine();
-			
-		
+	public static void stage2(String stage2Input) {
+
 		while (!stage2Input.contains("a")) {
 			System.out.println("'" + stage2Input + "'" + " does NOT contain a lowercase “a”");
 			System.out.print("\nPlease re-enter: ");
@@ -74,11 +59,7 @@ public class DataValidation {
 		}		
 	}
 	
-	public static void stage3() {
-		System.out.println("\n                  STAGE 3");
-		System.out.println("                 ---------");
-		System.out.println("Enter a STRING that contains between 5-15 charcaters and DOESN'T contain the letter “z”. ");
-		stage3Input = keyedInput.nextLine();
+	public static void stage3(String stage3Input) {
 		
 		while (!(stage3Input.length() >= 5 && stage3Input.length() <= 15) || stage3Input.contains("z")) {
 			if (!(stage3Input.length() >= 5 && stage3Input.length() <= 15)) {
@@ -93,12 +74,7 @@ public class DataValidation {
 		}		
 	}
 	
-	public static void stage4() {
-		
-		System.out.println("\n                  STAGE 4");
-		System.out.println("                 ---------");
-		System.out.println("Enter an INTEGER between 50 and 500.");		
-		stage4Input = realInt();
+	public static void stage4(String stage4Input) {
 		
 		int value = Integer.parseInt(stage4Input);
 		
@@ -109,23 +85,88 @@ public class DataValidation {
 			stage4Input = realInt();
 			value = Integer.parseInt(stage4Input);
 		}
-		
-		System.out.println("Everything works!");	
 	}
 	
+	public static void stage5(String stage5Input) {
+		
+		int value = Integer.parseInt(stage5Input);
+		
+		while (value >= 0) {
+			System.out.println("'" + value + "' is not a negative integer.");
+
+			System.out.print("\nPlease re-enter: ");
+			stage5Input = realInt();
+			value = Integer.parseInt(stage5Input);
+		}
+	}
+	
+	public static void stage6(String stage6Input) {
+		int value = Integer.parseInt(stage6Input);
+		
+		while (value <= 0 || value % 2 == 0 ) {
+			
+			if (value <= 0) {
+				System.out.println("'" + value + "' is not a positive integer.");
+			}
+			
+			if (value % 2 == 0) {
+				System.out.println("'" + value + "' is not a an odd number.");
+			}
+
+			System.out.print("\nPlease re-enter: ");
+			stage6Input = realInt();
+			value = Integer.parseInt(stage6Input);
+		}
+
+		
+		
+	}
 	
 	
 	public static void main(String[] args) {
 		
 		menu();
 		
-		stage1();
+		System.out.println("\n                  STAGE 1");
+		System.out.println("                 ---------");
+		System.out.print("Enter a STRING that has more than 6 letters: ");
+		stage1Input = keyedInput.nextLine();
+		stage1(stage1Input);
 		
-		stage2();
+		System.out.println("\n                  STAGE 2");
+		System.out.println("                 ---------");
+		System.out.println("Enter a STRING that contains at least one lowercase “a”. ");
+		stage2Input = keyedInput.nextLine();
+		stage2(stage2Input);
 		
-		stage3();
+		System.out.println("\n                  STAGE 3");
+		System.out.println("                 ---------");
+		System.out.println("Enter a STRING that contains between 5-15 charcaters and DOESN'T contain the letter “z”. ");
+		stage3Input = keyedInput.nextLine();
+		stage3(stage3Input);
 		
-		stage4();
+		System.out.println("\n                  STAGE 4");
+		System.out.println("                 ---------");
+		System.out.println("Enter an INTEGER between 50 and 500.");		
+		stage4Input = realInt();
+		stage4(stage4Input);
+		
+		System.out.println("\n                  STAGE 5");
+		System.out.println("                 ---------");
+		System.out.println("Enter a NEGATIVE INTEGER.");		
+		stage5Input = realInt();
+		stage5(stage5Input);
+		
+		System.out.println("\n                  STAGE 6");
+		System.out.println("                 ---------");
+		System.out.println("Stage 6: Enter a positive, odd integer.");
+		stage6Input = realInt();
+		stage6(stage6Input);
+		
+		System.out.println("\nCongratulations! You have completed the data validation test!");
+		System.out.println("nSee you around :-)");
+		
+		
 		
 
 		
